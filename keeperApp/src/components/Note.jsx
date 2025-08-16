dfbf265636b05b371d1621af286e79257fef0c42
Santosh Adhvaidh <101855566+Blank0210@users.noreply.github.com>
2025-08-16 18:55:42 +0530

    Add files via upload

---
+++ keeperApp/src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+    
+    return (
+        <div className="note">
+            <h1>
+                First Note
+            </h1>
+            <p>Hello There!</p>
+        </div>
+    )
+};
+
+export default Note;
---
+++ keeperApp/src/index.jsx
@@ -0,0 +1,14 @@
+
+
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in the styles.css file to apply styling.
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
